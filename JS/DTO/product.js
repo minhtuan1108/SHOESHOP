b4d6248b6581edf9b1df24a2f3575f043034a907
minhtuan1108 <minhtuan.1108tn@gamil.com>
2022-10-24 17:19:40 +0700

    Add landing page

---
+++ JS/DTO/product.js
@@ -0,0 +1,17 @@
+class product{
+
+    constructor(id, name, price, discount, disc, image, type){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.disc = disc;
+        this.image = image;
+        this.image = type;
+    }
+
+    showDemo() {
+        return ``
+    }
+
+}
---
+++ JS/index.js
@@ -0,0 +1,22 @@
+// const content = document.querySelector('#search-contain');
+
+
+// class Giay{
+//     constructor(name, price){
+//         this.name = name;
+//         this.price = price;
+//     }
+
+//     returnHtml(){
+//         return `
+//             <p>Day la doi giay ${this.name} co gia ${this.price}</p>
+//         `
+//     }
+// }
+
+// const list = [new Giay('Nike',1), new Giay('Adidas',2)];
+// let html = '';
+// list.forEach((item) => html += item.returnHtml());
+// content.innerHTML = html;
+
+
