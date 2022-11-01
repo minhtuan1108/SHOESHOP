262058ce6c19e46df30cf0b93267b911fb81702d
minhtuan1108 <minhtuan.1108tn@gamil.com>
2022-11-01 12:07:56 +0700

    update product page

--- JS/DTO/product.js
+++ JS/DTO/product.js
@@ -1,13 +1,15 @@
 class product{
 
-    constructor(id, name, price, discount, disc, image, type){
+    constructor(id, name, price, discount, disc, image, type_id, min_size, max_size){
         this.id = id;
         this.name = name;
         this.price = price;
         this.discount = discount;
         this.disc = disc;
         this.image = image;
-        this.image = type;
+        this.type_id = type_id;
+        this.min_size = min_size;
+        this.max_size = max_size;
     }
 
     showDemo() {
